559861b42fcd768c28e678f2013953bb55172875
Hsutra <96309579+Hsutra@users.noreply.github.com>
2024-02-16 11:06:34 +0300

    Update EuclideanAlgorithm.py

--- EuclideanAlgorithm.py
+++ EuclideanAlgorithm.py
@@ -1,9 +1,7 @@
-import math
-#Реализовать программный продукт решения диофантового
-#уравнения первой степени с помощью расширенного алгоритма
-#Евклида с указанием всех промежуточных результатов.
-# ax + bx = c
-
+'''Реализовать программный продукт решения диофантового
+уравнения первой степени с помощью расширенного алгоритма
+Евклида с указанием всех промежуточных результатов.
+'''
 def euclideanAlgorithm(a, b):
     if b == 0:
         return a, 1, 0
@@ -14,7 +12,7 @@ def euclideanAlgorithm(a, b):
 def resolveEquation(a, b, c):
     gcd, x, y = euclideanAlgorithm(a, b)
     if c % gcd != 0:
-        return "Решения нет"
+        return None
     else:
         x *= c // gcd
         y *= c // gcd
@@ -24,5 +22,8 @@ a = 5
 b = 7
 c = 14
 res = resolveEquation(a, b, c)
-print(f"Решение уравнения: {a}x + {b}y = {c} равно {res}.")
-
+if res:
+    commRes = f"x = {res[0]} + {b}k y = {res[1]} - {a}k"
+    print(f"Решение уравнения {a}x + {b}y = {c}:\nЧастное решение: x = {res[0]}, y = {res[1]}\nОбщее решение: {commRes}")
+else:
+    print("Решения нет")
